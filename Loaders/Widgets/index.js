a36b86b622a649417fcc5e1222dd30e534c0d332
Andriy Chaika <andriy@chaika.in.ua>
2014-10-17 18:22:27 +0300

    #300 widget pass ME inside

--- Loaders/Widgets/index.js
+++ Loaders/Widgets/index.js
@@ -10,20 +10,44 @@ var widgets = {};
  */
 function widget(widget_arguments) {
     //this will be the app.locals in view
+    if('ME' in widget_arguments) {
+        widget_arguments.options.ME = widget_arguments.ME;
+    }
     return this.renderTemplate(widget_arguments.view, widget_arguments.options);
+}
+
+/**
+ * Extends function with module arguments
+ * @param old_function
+ * @param module
+ */
+function wrapFunction(old_function, module) {
+
+    var ME = module.getFunctions();
+
+    return function() {
+        var res = old_function.apply(ME, arguments);
+        //If the function is real widget it should have view member
+        if(typeof res == "object") {
+            if('view' in res) {
+                res.ME = ME;
+            }
+        }
+        return res;
+    }
 }
 
 /**
  * Loads all widgets from given module
  * @param dir
  */
-exports.load = function(dir) {
+exports.load = function(dir, module) {
     //Include index.js file
     var data = require(dir);
 
     //Add each widget into widget list
     for(var key in data) {
-        widgets[key] = data[key];
+        widgets[key] = wrapFunction(data[key], module);
     }
     return widgets;
 };
